7ae3702921e1a77be98a34c56f7316f284c87375
SATAKE Naoya <naoya.satake@k-xgn.com>
2023-04-26 08:03:48 +0900

    実行ファイル更新

--- T23AJ001_xcap.sh
+++ T23AJ001_xcap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-TOOL=/home/xgntools/T2xAJ001/src/xcap_tool.py
-export PYTHONPATH=${PYTHONPATH}:/home/xgntools/T2xAJ001/
+TOOL=/home/xgntools/T23AJ001/src/xcap_tool.py
+export PYTHONPATH=${PYTHONPATH}:/home/xgntools/T23AJ001/
 
-# execute nf_registration_tool.py
+# execute xcap_tool.py
 python3 -O ${TOOL} "${@}"
 EXIT_CODE=${?}
 
