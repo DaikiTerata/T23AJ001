28d19b7b82fd203d0de6aee8d0a24064adf64565
SATAKE Naoya <naoya.satake@k-xgn.com>
2023-05-11 16:35:49 +0900

    bugfix:網閉塞管理DBから閉塞中NF名のリストを取得する処理を追加

--- T23AJ001_xcap.sh
+++ T23AJ001_xcap.sh
@@ -3,8 +3,18 @@
 TOOL=/home/xgntools/T23AJ001/src/xcap_tool.py
 export PYTHONPATH=${PYTHONPATH}:/home/xgntools/T23AJ001/
 
+HEISOKUDB_TOOL=/home/xgntools/T8AJ001/T8AJ001_heisoku-db.sh
+HEISOKU_LIST=$(${HEISOKUDB_TOOL} LISTW)
+i=0
+while [ "$1" != "" ] && [[ $1 != *"--"* ]]
+do
+    ARGV[i]=$1
+    shift
+    i=$((i + 1))
+done
+
 # execute xcap_tool.py
-python3 -O ${TOOL} "${@}"
+python3 -O ${TOOL} "${ARGV[@]}" "${HEISOKU_LIST}" "${@}"
 EXIT_CODE=${?}
 
 exit ${EXIT_CODE}
